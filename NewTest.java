54d17b0b7eb80f5fa814aab7022e5d3ee4079bd1
Daniel Joe <hrlddaniel@gmail.com>
2017-03-15 19:37:33 -0500

    Update with sleep times to allow click to happen

--- NewTest.java
+++ NewTest.java
@@ -10,57 +10,59 @@ import org.testng.annotations.Test;
  
 public class NewTest {
   @Test
-  public void f() {
+  public void f() throws InterruptedException {
               
                 
-                       // NOTE: Substitute your own saved location of the geckodriver.exe as you saved previously for the name here below
-                                             System.setProperty("webdriver.gecko.driver","C:\\Test\\drivers\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+	// NOTE: Substitute your own saved location of the geckodriver.exe as you saved previously for the name here below
+	System.setProperty("webdriver.gecko.driver","/media/jd/storage/linux/tools/drivers/geckodriver");
               
-                                             WebDriver driver = new FirefoxDriver();
+  	WebDriver driver = new FirefoxDriver();
               
-                                             driver.get("http://www.google.com");
+  	driver.get("http://www.google.com");
               
-                                             System.out.println("Application title is ============= "+driver.getTitle());
+  	System.out.println("Application title is ============= "+driver.getTitle());
                                             
-                                             WebElement element1 = driver.findElement(By.xpath(".//*[@id='lst-ib']"));
+  	WebElement element1 = driver.findElement(By.xpath(".//*[@id='lst-ib']"));
                                             
-                                             element1.click();
+  	element1.click();
                                             
-                                             //driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+  	//driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
                                             
-                                             WebElement element2 = driver.findElement(By.partialLinkText("About"));
+  	WebElement element2 = driver.findElement(By.partialLinkText("About"));
               
-                                             element2.click();
+  	element2.click();
+  	
+  	Thread.sleep(3000);
                                             
-                                             driver.quit();
+  	driver.quit();
               
-                   }
+  }
   @Test
-  public void f2() {
+  public void f2() throws InterruptedException {
               
-                
-                       // NOTE: Substitute your own saved location of the geckodriver.exe as you saved previously for the name here below
-                                             System.setProperty("webdriver.gecko.driver","C:\\Test\\drivers\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+	  // NOTE: Substitute your own saved location of the geckodriver.exe as you saved previously for the name here below
+      System.setProperty("webdriver.gecko.driver","/media/jd/storage/linux/tools/drivers/geckodriver");
               
-                                             WebDriver driver = new FirefoxDriver();
+      WebDriver driver = new FirefoxDriver();
               
-                                             driver.get("http://www.arduino.cc");
+      driver.get("http://www.arduino.cc");
               
-                                             System.out.println("Application title is ============= "+driver.getTitle());
+      System.out.println("Application title is ============= "+driver.getTitle());
                                             
-                                             WebElement element1 = driver.findElement(By.xpath(".//*[@id='navWrapper']/nav/section/ul[1]/li[3]/a"));
+      WebElement element1 = driver.findElement(By.xpath(".//*[@id='navWrapper']/nav/section/ul[1]/li[3]/a"));
                                             
-                                             element1.click();
+      element1.click();
                                             
-                                             //driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+      //driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
                                             
-                                             WebElement element2 = driver.findElement(By.partialLinkText("Support"));
+      WebElement element2 = driver.findElement(By.partialLinkText("Support"));
               
-                                             element2.click();
+      element2.click();
+      
+      Thread.sleep(10000);
                                             
-                                             driver.quit();
+      driver.quit();
               
-                   }
+  }
 }
- 
  
